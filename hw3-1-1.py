0a7f6f981389d17cf69d1eb177a84a921235cf19
p22strautz <p22strautz@fairfieldprep.org>
2021-09-28 10:20:19 -0400

    finish hw3-1

---
+++ hw3-1-1.py
@@ -0,0 +1,10 @@
+# Author: SCT (ADMG) 9/28/21
+
+team1_wins = input("Enter team 1s wins:\n") * 3
+
+team1_ties = input("Enter team 1s ties:\n")
+
+team2_wins = input("Enter team 2s wins:\n") * 3
+
+team2_ties = input("Enter team 2s ties:\n")
+
